d4a2c119744800a44346a936e99418907cc31ccf
Ritsuki Otsuka <y.tsuka0728@icloud.com>
2020-07-24 00:01:56 -0300

    Updates

---
+++ precache-manifest.1e599f2dc7d46a24eb8e85337236666b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8803fe97117d5429013a8f76aa00f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74f9e1f03fd49bd3d959",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f9e1f03fd49bd3d959",
+    "url": "/static/js/main.eaaba61a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
